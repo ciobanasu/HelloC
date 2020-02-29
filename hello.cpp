9c403dd2d99fc838e61f8626c2ae9107c929cb4a
Ciobanasu Ion <nikusor.94@gmail.com>
2020-03-01 01:49:32 +0200

    Some changes

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,13 @@ using namespace std;
 int main() {
      cout << "Hello 1" << endl;
 
+     for(int i = 0; i <= a; i++){
+          cout << "i is: " << i << endl; 
+     }
+
+     cout << "Hello 1" << endl;
+
+     cout << "Hello 1" << endl;
+
      return "hello";
 }
