ab8973576b3a5911c2834bee6f5b6b3046ad0257
Ciobanasu Ion <nikusor.94@gmail.com>
2020-06-14 02:15:59 +0300

    Changed for loop. Deleted one line.

--- hello.cpp
+++ hello.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 
 int main() {
-     cout << "Hello 1" << endl;
 
      for(int i = 0; i <= a; i++){
-          cout << "i is: " << i << endl; 
+          cout << "i = " << i << endl; 
      }
 
      for(int j = 0; j <= 5; j++) {
           cout << "j = " << j << endl;
      }
 
-     return "hello";
+     return 0;
 }
